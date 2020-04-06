8478f3bf87a5bb323d235d79805a895e0ce6f868
Andrea Bottini <andrea.bottini22@gmail.com>
2020-04-06 16:35:49 +0200

    Up

--- resources/views/autos/index.blade.php
+++ resources/views/autos/index.blade.php
@@ -1,10 +1,40 @@
 @extends('layouts.layout')
 @section('header')
-    <h1>AUTOBOOLSCOUT24</h1>
+        <div class="header">
+            <nav class="navbar navbar-expand-lg navbar-light bg-light">
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <a class="navbar-brand" href="#">AutoBoolScout24</a>
+
+                <div class="collapse navbar-collapse" id="navbarTogglerDemo03">
+                    <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+                        <li class="nav-item active">
+                            <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Dicono di Noi</a>
+                        </li>
+                       {{-- <li class="nav-item">
+                            <a class="nav-link disabled" href="#">Disabled</a>
+                        </li>--}}
+                    </ul>
+                    <form class="form-inline my-2 my-lg-0">
+                        <button class="btn btn-success"><a href="{{Route('autos.create')}}">Inserisci la nuova auto</a></button>
+                        <button class="btn btn-success"><a href="{{Route('autos.create')}}">Login</a></button>
+                    </form>
+                </div>
+            </nav>
+            <div class="jumbotron">
+                <img src="..." class="img-fluid" alt="Responsive image">
+            </div>
+        </div>
 @endsection
 @section('main')
+
 <div class="autos">
-    <button class="btn btn-success"><a href="{{Route('autos.create')}}">Inserisci la nuova auto</a></button>
+
+
 
     @foreach ($autos as $auto)
     <ul class="list-group">
@@ -14,17 +44,17 @@
         <li class="list-group-item">Prezzo: {{$auto->price}}</li>
         <li class="list-group-item">Descrizione: {{$auto->description}}</li>
     </ul>
-    @endforeach   
+    @endforeach
 </div>
- 
+
 @endsection
 @section('scripts')
-    
+
 @endsection
 @section('footer')
-    
+
 @endsection
 
 
 
-<input type="button" 
+<input type="button"
